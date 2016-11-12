5be502592b2c55daad493fba9ed2f7887cd5d1fb
Roger Nkosi <rogermphile1991@gmail.com>
2016-11-12 13:57:08 +0200

    back up before drastic changes

--- app/src/main/java/nkosi/roger/cefupsacademy/HomeFragment.java
+++ app/src/main/java/nkosi/roger/cefupsacademy/HomeFragment.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class HomeFragment extends Fragment {
@@ -23,6 +24,7 @@ public class HomeFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
     private LinearLayoutManager mLinearLayoutManager;
+    private List<MyCefups> persons;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -37,14 +39,135 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
 
         mRecyclerView = (RecyclerView)view.findViewById(R.id.my_cefups);
-        mLinearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        mLinearLayoutManager = new LinearLayoutManager(getActivity().getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
         mRecyclerView.setHasFixedSize(true);
+        initializeData();
+
+        RVAdapter adapter = new RVAdapter(persons);
+        mRecyclerView.setAdapter(adapter);
+
 
         return view;
     }
 
 
+    public class MyCefups {
+        private String name;
+        private int photoId;
 
+        public MyCefups(String name, int photoId) {
+            this.name = name;
+            this.photoId = photoId;
+        }
+    }
+
+    // This method creates an ArrayList that has three Person objects
+    // Checkout the project associated with this tutorial on Github if
+    // you want to use the same images.
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new MyCefups("Assignments", R.drawable.ic_assignment_turned_in));
+        persons.add(new MyCefups("Class Tests", R.drawable.ic_assignment));
+        persons.add(new MyCefups("My Schedule", R.drawable.ic_date_range ));
+        persons.add(new MyCefups("My Account", R.drawable.ic_account_box));
+        persons.add(new MyCefups("Announcements", R.drawable.ic_announcement));
+    }
+
+    public class RVAdapter extends RecyclerView.Adapter<RVAdapter.CefupsViewHolder>{
+
+        List<MyCefups> list;
+
+        public RVAdapter(List<MyCefups> list) {
+            this.list = list;
+        }
+
+        /**
+         * Called when RecyclerView needs a new {@link ViewHolder} of the given type to represent
+         * an item.
+         * <p>
+         * This new ViewHolder should be constructed with a new View that can represent the items
+         * of the given type. You can either create a new View manually or inflate it from an XML
+         * layout file.
+         * <p>
+         * The new ViewHolder will be used to display items of the adapter using
+         * {@link #onBindViewHolder(ViewHolder, int, List)}. Since it will be re-used to display
+         * different items in the data set, it is a good idea to cache references to sub views of
+         * the View to avoid unnecessary {@link View#findViewById(int)} calls.
+         *
+         * @param parent   The ViewGroup into which the new View will be added after it is bound to
+         *                 an adapter position.
+         * @param viewType The view type of the new View.
+         * @return A new ViewHolder that holds a View of the given view type.
+         * @see #getItemViewType(int)
+         * @see #onBindViewHolder(ViewHolder, int)
+         */
+        @Override
+        public CefupsViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.my_cefups_row, parent, false);
+            return new CefupsViewHolder(v);
+        }
+
+        /**
+         * Called by RecyclerView to display the data at the specified position. This method should
+         * update the contents of the {@link ViewHolder#itemView} to reflect the item at the given
+         * position.
+         * <p>
+         * Note that unlike {@link ListView}, RecyclerView will not call this method
+         * again if the position of the item changes in the data set unless the item itself is
+         * invalidated or the new position cannot be determined. For this reason, you should only
+         * use the <code>position</code> parameter while acquiring the related data item inside
+         * this method and should not keep a copy of it. If you need the position of an item later
+         * on (e.g. in a click listener), use {@link ViewHolder#getAdapterPosition()} which will
+         * have the updated adapter position.
+         * <p>
+         * Override {@link #onBindViewHolder(ViewHolder, int, List)} instead if Adapter can
+         * handle efficient partial bind.
+         *
+         * @param holder   The ViewHolder which should be updated to represent the contents of the
+         *                 item at the given position in the data set.
+         * @param position The position of the item within the adapter's data set.
+         */
+        @Override
+        public void onBindViewHolder(CefupsViewHolder holder, int position) {
+            holder.textView.setText(list.get(position).name);
+            holder.imageView.setImageResource(list.get(position).photoId);
+        }
+
+        /**
+         * Called by RecyclerView when it starts observing this Adapter.
+         * <p>
+         * Keep in mind that same adapter may be observed by multiple RecyclerViews.
+         *
+         * @param recyclerView The RecyclerView instance which started observing this adapter.
+         * @see #onDetachedFromRecyclerView(RecyclerView)
+         */
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        /**
+         * Returns the total number of items in the data set held by the adapter.
+         *
+         * @return The total number of items in this adapter.
+         */
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        public class CefupsViewHolder extends RecyclerView.ViewHolder{
+
+            TextView textView;
+            ImageView imageView;
+
+            public CefupsViewHolder(View itemView) {
+                super(itemView);
+                textView = (TextView)itemView.findViewById(R.id.my_cefups_txt_view);
+                imageView = (ImageView)itemView.findViewById(R.id.my_cefups_image);
+            }
+        }
+    }
 
 }
---
+++ app/src/main/java/nkosi/roger/cefupsacademy/MyCefups.java
@@ -0,0 +1,7 @@
+package nkosi.roger.cefupsacademy;
+
+import java.util.List;
+
+/**
+ * Created by lulu on 11/11/2016.
+ */
